9acbee4d47ad6e87299a7cbda277da4dc2a4f0c1
lukashuk542-prog <lukashuk542@gmail.com>
2026-08-12 15:39:08 +0400

    new site bug fixes

--- app/app.py
+++ app/app.py
@@ -7,6 +7,9 @@ from app.schemas import CommandRequest, CommandResponse
 from app.security import get_user_auth_token
 from dotenv import  load_dotenv
 from os import getenv
+import soundcard
+import asyncio
+import numpy as np
 load_dotenv()
 app = FastAPI()
 
@@ -45,7 +48,7 @@ async def ws(websocket: WebSocket, token: str):
 
     if getenv("API_SECRET_KEY") != token:
         await websocket.close(code=1008)
-        return "error"
+        return
 
     await websocket.accept()
 
@@ -68,6 +71,57 @@ async def show_stream(token: str):
     if getenv("API_SECRET_KEY") != token:
         raise HTTPException(status_code=401,detail="Unauthorized")
     return StreamingResponse(generate_frames(),media_type="multipart/x-mixed-replace; boundary=frame")
+
+
+
+
+
+
+
+@app.websocket("/ws/audio")
+async def ws_audio(websocket: WebSocket, token: str):
+    if getenv("API_SECRET_KEY") != token:
+        await websocket.close(code=1008)
+        return "error"
+
+
+    await websocket.accept()
+
+    SAMPLERATE = 48000
+    CHUNK_SIZE = 1024
+    # Получаем дефолтный динамик
+    default_speaker = soundcard.default_speaker()
+    # получаем все микрофоны в виде объекта с микрофонами с глобальным прослушиванием loopback
+    mics = soundcard.all_microphones(include_loopback=True)
+    mic = None
+    for m in mics:
+        if m.name == default_speaker.name:
+            mic = m
+            break
+
+    if not mic:
+        await websocket.close(code=1011)
+        return
+    # Открываем микрофон для записи
+    with mic.recorder(samplerate=SAMPLERATE,channels=1) as rec:
+        while True:
+            try:
+                # data - массив numpy float32
+                data = await asyncio.to_thread(rec.record,numframes=CHUNK_SIZE)
+                data_int16 = (data * 32767).astype(np.int16)
+                audio_bytes = data_int16.tobytes()
+                await  websocket.send_bytes(audio_bytes)
+            except WebSocketDisconnect as e:
+                print(e)
+                break
+
+            except Exception as e:
+                print(e)
+                break
+
+
+
+
 
 
 
